d88e1f01f42afe05664a87e6a2ce6adf28ff3895
KnightinGale9 <zhongooi0412@gmail.com>
2023-02-13 16:02:12 -0800

    Zhong Ooi - updated analysis script

--- codenames/result_analysis_script_improved.py
+++ codenames/result_analysis_script_improved.py
@@ -6,32 +6,40 @@ seed=500
 def run():
     guesserlist = ["players.guesser_glove.AIGuesser","players.guesser_w2v.AIGuesser",
                     "players.guesser_w2vglove.AIGuesser","players.guesser_transformer.AIGuesser"]
-        # ,"players.guesser_wn_jcn.AIGuesser",
-        #            "players.guesser_wn_lch.AIGuesser","players.guesser_wn_lin.AIGuesser",
-        #            "players.guesser_wn_path.AIGuesser","players.guesser_wn_res.AIGuesser",
-        #            "players.guesser_wn_wup.AIGuesser"]
+
     cmlist= ["players.codemaster_glove_03.AICodemaster",
             "players.codemaster_glove_05.AICodemaster","players.codemaster_glove_07.AICodemaster",
             "players.codemaster_w2v_03.AICodemaster", "players.codemaster_w2v_05.AICodemaster",
              "players.codemaster_w2v_07.AICodemaster","players.codemaster_w2vglove_03.AICodemaster",
              "players.codemaster_w2vglove_05.AICodemaster","players.codemaster_w2vglove_07.AICodemaster"]
-    counter=seed
-    for i in range(runtime):
-        # for codemaster in cmlist:
+    for temp in range(4):
+        counter=seed
         for guesser in guesserlist:
-            gamerun("players.codemaster_transformer.AICodemaster", guesser,counter)
-        # for codemaster in cmlist:
-        #     gamerun(codemaster,"players.guesser_glove.AIGuesser", counter)
-        counter += 50
+            for i in range(runtime):
+                gamerun("players.codemaster_transformer.AICodemaster", guesser,counter,temp)
+            counter += 50
+        counter=seed
+        for codemaster in cmlist:
+            for i in range(runtime):
+                gamerun(codemaster, "players.guesser_transformer.AIGuesser", counter,temp)
+            counter += 50
 
-def gamerun(codemaster, guesser, seed):
+def gamerun(codemaster, guesser, seed,runnum):
     builder=["python", "run_game.py", codemaster, guesser]
     if "w2v" in codemaster or "w2v" in guesser:
         builder.append("--w2v")
         builder.append("players/GoogleNews-vectors-negative300.bin")
     if "glove" in codemaster or "glove" in guesser:
         builder.append("--glove")
-        builder.append("players/glove/glove.6B.300d.txt")
+        if runnum == 0:
+            builder.append("players/glove/glove.6B.300d.txt")
+        elif runnum == 1:
+            builder.append("players/glove/glove.6B.200d.txt")
+        elif runnum == 2:
+            builder.append("players/glove/glove.6B.100d.txt")
+        elif runnum == 3:
+            builder.append("players/glove/glove.6B.50d.txt")
+
     builder.append("--no_print")
     builder.append("--seed")
     builder.append(str(seed))
